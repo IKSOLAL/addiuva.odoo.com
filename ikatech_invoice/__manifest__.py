b5b40b63fd5601caf7a328c0525c2227f712d4f1
ivanporras <ivanporras>
2022-02-24 11:03:29 -0600

    FIX invoice format

---
+++ ikatech_invoice/__manifest__.py
@@ -0,0 +1,25 @@
+##############################################################################
+#
+#    Copyright (C) 2021  Ikatech  (https://ikatechsolutions.com/)
+#    All Rights Reserved.
+#
+##############################################################################
+{
+    'name': 'Ikatech Invoice Format',
+    'version': '14.0.1.0.1',
+    'category': 'Accounting',
+    'summary': "Ikatech Invoice Format",
+    'author': "Ikatech",
+    'website': 'https://ikatechsolutions.com/',
+    'license': 'AGPL-3',
+    'depends': [
+
+    ],
+    'data': [
+        'report/invoice_report.xml',
+    ],
+    'installable': True,
+
+    'odoo-license': 'EE',
+
+}
