f2999f337ea908a756decd2e7794f9529df1a76f
ivanporras <iporras@ikatechsolutions.com>
2022-08-07 18:52:00 -0500

    [ADD] Dev cheques for cr

---
+++ account_cheques/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    "name": "Account - Manual Cheques",
+    "version": "15.0.0.0.0",
+    "category": "Accounting",
+    "summary": "Add check number to register payment",
+    "author": "Ikatech Solutions",
+    "maintainers": ["ivanporras"],
+    "website": "https://ikatechsolutions.com",
+    "depends": ["account"],
+    "data": [
+        "views/account_payment_register_view.xml",
+    ],
+    "installable": True,
+}
---
+++ account_cheques/models/account_payment_register.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields,api
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    check_number = fields.Char(string="Número de cheque")
+
+    show_check_number = fields.Boolean(string="Show Check Number")
+
+    @api.onchange('payment_method_line_id')
+    def _compute_show_check_number(self):
+        for wizard in self:
+            wizard.show_check_number = False
+            if wizard.payment_method_line_id.name == 'Cheques' or wizard.payment_method_line_id.name == 'Checks':
+                wizard.show_check_number = True
+
+    def _create_payments(self):
+        payments = super()._create_payments()
+        for p in payments:
+            p.check_number = self.check_number
+        return payments
+
