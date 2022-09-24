5e25974ad0e03950564cdda3a6ab86ace873ea56
ivanporras <iporras@ikatechsolutions.com>
2022-09-23 23:26:19 -0500

    [IMP] Improve report invoice Chile

---
+++ l10n_cl_extended/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'l10n_cl_extended',
+    'version': '15.0.1.0.0',
+    'category': '',
+    'summary': "",
+    'author': "Ikatech Solutions",
+    'website': '',
+    'license': 'AGPL-3',
+    'description': '''Extension del modulo l10n_cl''',
+    'depends': [
+
+        'l10n_cl',
+
+    ],
+    'data': [
+        'views/report_invoice_with_pay.xml',
+
+    ],
+
+    'installable': True,
+}
