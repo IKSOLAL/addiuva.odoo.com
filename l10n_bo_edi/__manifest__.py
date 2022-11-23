5364f2bfbe2f29fabb60f120e521b389c89eb1d6
BDuchen <brad.duchen@alphasys.com.bo>
2022-11-23 00:14:34 -0400

    [FIX] l10n_bo_edi v3.0

--- l10n_bo_edi/__manifest__.py
+++ l10n_bo_edi/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': """Bolivia - E-invoicing""",
-    "version": "14.0.2.9",
+    "version": "14.0.3.0",
     #     'description': """
     # EDI Bolivian Localization
     # ========================
