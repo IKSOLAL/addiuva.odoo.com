b9b8f09b19b666c6df4c9df00fe4d4fa50d52762
ivanporras <iporras@ikatechsolutions.com>
2023-03-03 10:36:37 -0600

    [IMP] Improve bolivia 2222333

---
+++ l10n_bo_edi_extended/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': """Bolivia - E-invoicing Extended""",
+    "version": "1.0",
+    'author': "Ivan Porras",
+    'category': 'Accounting/Localizations',
+    'depends': ['l10n_bo_edi'],
+    'data': [
+        'reports/invoice_report_template_extended.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
