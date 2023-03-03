90c6e4d8df07ad790d5bed041d1e767c75425d3e
ivanporras <iporras@ikatechsolutions.com>
2023-03-03 10:07:14 -0600

    [ADD] Add invoice bolivia extended

---
+++ l10n_bo_edi_extended/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': """Bolivia - E-invoicing Extended""",
+    "version": "1.0",
+
+    'author': "Ivan Porras",
+    'category': 'Accounting/Localizations',
+    'depends': ['l10n_bo_edi_extended',],
+    'data': [
+        'reports/invoice_report_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
