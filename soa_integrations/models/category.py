2c5abd9ce6e7660abb56640a313b041b86c13528
ivanporras <iporras@ikatechsolutions.com>
2023-03-24 09:49:19 -0600

    [IMP] Soa 3

--- soa_integrations/models/category.py
+++ soa_integrations/models/category.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 class category(models.Model):
     _inherit = ["product.category"]
 
-    cod_soa = fields.Integer(string="Código SOA", required=True)
+    cod_soa = fields.Integer(string="Código SOA", required=True, default=0)
     company_id = fields.Many2one(comodel_name='res.company', string="Empresa",
                                  default=lambda self: self.env['res.company'].browse(self.env['res.company']._company_default_get('ik_integrations_group')))
