4908aac589d14c3ca9e6287425c1cfb498c8c6ee
ivanporras <iporras@ikatechsolutions.com>
2023-03-24 09:35:08 -0600

    [IMP] Improve SOA integrations codigo SOA

--- soa_integrations/models/product_template.py
+++ soa_integrations/models/product_template.py
@@ -4,7 +4,7 @@ class product_template(models.Model):
     _inherit = ["product.template"]
 
     plan_servicio_ids = fields.Many2many("product.services.plans", string="Planes")
-    cod_soa = fields.Integer(string='Código SOA', required=True)
+    cod_soa = fields.Integer(string='Código SOAs', required=True)
     company_id = fields.Many2one(comodel_name='res.company', string="Empresa",
                                  default=lambda self: self.env['res.company'].browse(
                                      self.env['res.company']._company_default_get('ik_integrations_group')))
