f4ae3e8880253ded9f50c8390d10d18a098a0290
ivanporras <iporras@ikatechsolutions.com>
2023-05-31 14:07:50 -0600

    [ADD] L10n eg edi eta

---
+++ l10n_eg_edi_eta_extended/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Egipto',
+    'summary': '''
+        Redondear la divisa a cinco decimales, solo para EGP
+    ''',
+    'author': 'Ivan Porras',
+    'category': 'Contabilidad',
+    'version': '1.0.0',
+    'depends':['l10n_eg_edi_eta'],
+    'data': [
+    ]
+}
