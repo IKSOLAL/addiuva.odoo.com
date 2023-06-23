86d6fe3091a3e0a316ddde0bff5d897262cfe1ea
ivanporras <iporras@ikatechsolutions.com>
2023-06-23 13:31:52 -0500

    [IMP] Purchase order SOA Plan Service

--- soa_integrations/models/purchase_order.py
+++ soa_integrations/models/purchase_order.py
@@ -6,4 +6,4 @@ class PurchaseOrder(models.Model):
     support_num_soa = fields.Char(string="SOA No. Soporte")
     cod_soa = fields.Integer(string='Código SOA', required=True, default=0)
     record_num_soa = fields.Char(string="SOA No. Expediente")
-
+    product_service_plan_id = fields.Many2one(comodel_name="product.services.plans",string="Plan-Servicio")
