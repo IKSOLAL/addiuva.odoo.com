a9bf9563c82d1fe15b9452409a402e39f30adf41
cesargtz <cgutierrez@ikatechsolutions.com>
2023-08-31 19:25:01 +0000

    [add] COA Reports Extend

---
+++ l10n_mx_reports_extend/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_mx_reports_extend",
+
+    'author': "íkatech",
+
+    'category': 'account',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','l10n_mx_reports'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ]
+}
