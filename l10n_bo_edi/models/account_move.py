23a8413b236f4ba1e3a9878e5243eea49c38411d
Ivan Porras <ivanporras@Ivans-MacBook-Air.local>
2023-10-04 23:26:05 -0500

    [FIX] Bolivia account move post

--- l10n_bo_edi/models/account_move.py
+++ l10n_bo_edi/models/account_move.py
@@ -1993,48 +1993,49 @@ class AccountMove(models.Model):
     def action_post(self):
         cancel_flag = int(self.env['bo_edi_params'].search(
                                             [('name', '=', 'CANCEL_ACTIVE')]).value) ##Flag para facturas de reversión automáticas
-        
-        if self.country_code == 'BO':
-            if not cancel_flag:
-                if (self.journal_id.type == 'sale' or self.journal_id.type == 'purchase'):
-                    if self.inv_type: ## Si es factura de venta
-                        if self.e_billing: ## Si la modalidad es electronica...
-                            if not self.external_con: ## Si la conexion al SIN será por Odoo o por backend .NET
-                                if not self.invoice_date:
-                                    raise ValidationError("The invoice date field is required")
-                                if self.is_cancelled:
-                                    raise ValidationError("The current invoice has been cancelled, we cannot resend it to SIN")
+        #@ivan_porras: Ikatech Code
+        for record in self:
+            if record.country_code == 'BO':
+                if not cancel_flag:
+                    if (record.journal_id.type == 'sale' or record.journal_id.type == 'purchase'):
+                        if record.inv_type: ## Si es factura de venta
+                            if record.e_billing: ## Si la modalidad es electronica...
+                                if not record.external_con: ## Si la conexion al SIN será por Odoo o por backend .NET
+                                    if not record.invoice_date:
+                                        raise ValidationError("The invoice date field is required")
+                                    if record.is_cancelled:
+                                        raise ValidationError("The current invoice has been cancelled, we cannot resend it to SIN")
+                                    else:
+                                        res_conectivity = record.check_conectivity()
+                                        if res_conectivity:
+                                            record.is_confirmed = True
+                                            current_status = int(record.env['bo_edi_params'].search(
+                                                    [('name', '=', 'ONLINE')]).value)
+                                            internet_status = int(record.env['bo_edi_params'].search(
+                                                    [('name', '=', 'DEMO_INT_STATUS')]).value)
+
+                                            if current_status:
+                                                record.send_email_with_attachment(0)
+                                            elif internet_status:
+                                                record.send_email_with_attachment(1)
+
+                                            record.print_report()
                                 else:
-                                    res_conectivity = self.check_conectivity()
-                                    if res_conectivity:
-                                        self.is_confirmed = True
-                                        current_status = int(self.env['bo_edi_params'].search(
-                                                [('name', '=', 'ONLINE')]).value)
-                                        internet_status = int(self.env['bo_edi_params'].search(
-                                                [('name', '=', 'DEMO_INT_STATUS')]).value)
+                                    new_inv = record.post_invoice(0)
+                                    if new_inv[0]:
+                                        record.send_email_with_attachment(0 , new_inv[1][0]["xml"])
+                                    elif new_inv[0] == 0:
+                                        raise ValidationError("The current invoice is already posted in SIN")
 
-                                        if current_status:
-                                            self.send_email_with_attachment(0)
-                                        elif internet_status:
-                                            self.send_email_with_attachment(1)
-
-                                        self.print_report()
-                            else:
-                                new_inv = self.post_invoice(0)
-                                if new_inv[0]:
-                                    self.send_email_with_attachment(0 , new_inv[1][0]["xml"])
-                                elif new_inv[0] == 0:
-                                    raise ValidationError("The current invoice is already posted in SIN")
-                                    
-                        else: ## Si la modalidad es estandar
-                            nit = self.partner_id.vat
-                            currency_name = self.invoice_line_ids.mapped('sale_line_ids').order_id.pricelist_id.currency_id.name
-                            if not nit:
-                                raise ValidationError("There is no VAT/NIT for the client company")
-                            else:
-                                self.generate_control_code()
-                                self.is_confirmed = True
-                                self.print_report()
+                            else: ## Si la modalidad es estandar
+                                nit = record.partner_id.vat
+                                currency_name = record.invoice_line_ids.mapped('sale_line_ids').order_id.pricelist_id.currency_id.name
+                                if not nit:
+                                    raise ValidationError("There is no VAT/NIT for the client company")
+                                else:
+                                    record.generate_control_code()
+                                    record.is_confirmed = True
+                                    record.print_report()
         super(AccountMove, self).action_post()
 
     ########### HELPERS ################
