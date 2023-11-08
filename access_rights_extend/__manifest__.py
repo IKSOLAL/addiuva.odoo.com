06d1d9d3933cbfba75aa2455e4de2d8a48463e16
César Gtz <cesareomargtz@gmail.com>
2023-11-08 16:21:56 -0600

    Custom Permissions Module

---
+++ access_rights_extend/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Access Rights Extend",
+
+    'summary': """
+       Set a default condition so that only members of the group can create master data.""",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','account','contacts','stock','analytic'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'security/res_groups.xml',
+        'views/views.xml',
+    ],
+
+}
