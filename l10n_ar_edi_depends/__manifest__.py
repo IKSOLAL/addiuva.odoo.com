de36f90837d6cdf5afa6028249ab0eb1ad8523dc
Ivan Porras <ivanporras@Ivans-MacBook-Air.local>
2024-01-31 22:54:38 -0600

    AUTO INSTALL L10N AR EDI

---
+++ l10n_ar_edi_depends/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_ar_edi_depends",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['l10n_ar_edi'],
+
+    # always loaded
+    'data': [
+    ],
+    'auto_install': True,
+    'application': False,
+    'installable': True,
+
+}
