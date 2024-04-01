2352e7b4d33c8974ca8f3e24fdaa365d6d8b9bc1
Ivan Porras <ivanporras@Ivans-MacBook-Air.local>
2024-04-01 12:17:30 -0600

    [RMV] Remove RUC validation for Ecuatorian

---
+++ l10n_ec_extended/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'l10n_ec_extended',
+    'version': '15.0.1.0.0',
+    'category': '',
+    'summary': "",
+    'author': "Ikatech Solutions",
+    'website': '',
+    'license': 'AGPL-3',
+    'description': '''Extension del modulo l10n_ec''',
+    'depends': [
+
+        'l10n_ec',
+
+    ],
+    'data': [
+
+    ],
+
+    'installable': True,
+}
