fa6fa7734e10cb6da93e0bb21e320aafe6326237
Ivan Porras <ivanporras@Ivans-MacBook-Air.local>
2024-05-14 19:19:15 -0600

    [IMP] L10N_CO_EDI_JORELS_EXTENDED

--- l10n_co_edi_jorels_extended/models/account_move.py
+++ l10n_co_edi_jorels_extended/models/account_move.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools.sql import column_exists, create_column
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     flag_colombia = fields.Boolean(string="Colombia", compute="_compute_flag_colombia")
+
+    def _auto_init(self):
+        if not column_exists(self.env.cr, "account_move", "ei_type_document"):
+            create_column(self.env.cr, "account_move", "ei_type_document", "varchar")
+        if not column_exists(self.env.cr, "account_move", "ei_amount_tax_withholding_company"):
+            create_column(self.env.cr, "account_move", "ei_amount_tax_withholding_company", "varchar")
+        if not column_exists(self.env.cr, "account_move", "ei_amount_tax_no_withholding_company"):
+            create_column(self.env.cr, "account_move", "ei_amount_tax_no_withholding_company", "varchar")
+        if not column_exists(self.env.cr, "account_move", "ei_amount_total_no_withholding_company"):
+            create_column(self.env.cr, "account_move", "ei_amount_total_no_withholding_company", "varchar")
+        if not column_exists(self.env.cr, "account_move", "ei_amount_excluded_company"):
+            create_column(self.env.cr, "account_move", "ei_amount_excluded_company", "varchar")
+
+        return super()._auto_init()
 
     @api.depends('partner_id')
     def _compute_flag_colombia(self):
