d58fbee73212bc1b8f7cb475078f06a69eef663d
Ivan Porras <ivanporras@Ivans-MacBook-Air.local>
2024-10-14 13:32:59 -0600

    [FIX] Fix General Ledger

--- account_reports_extend/models/account_reports.py
+++ account_reports_extend/models/account_reports.py
@@ -3,6 +3,8 @@
 
 from odoo import models, api, fields, _
 from odoo.tools.misc import format_date, get_lang
+import io
+from odoo.tools.misc import xlsxwriter
 
 class ReportAccountAgedPartnerInherit(models.AbstractModel):
     _inherit = "account.aged.partner"
@@ -130,3 +132,113 @@ class ReportAccountAgedPartnerInherit(models.AbstractModel):
                 self._field_column('currency_id'),
             ]
         return columns
+
+#FIX General Ledger
+class AccountReport(models.AbstractModel):
+    _inherit = 'account.report'
+
+    def get_xlsx(self, options, response=None):
+        self = self.with_context(self._set_context(options))
+        output = io.BytesIO()
+        workbook = xlsxwriter.Workbook(output, {
+            'in_memory': True,
+            'strings_to_formulas': False,
+        })
+        sheet = workbook.add_worksheet(self._get_report_name()[:31])
+
+        date_default_col1_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': 2, 'num_format': 'yyyy-mm-dd'})
+        date_default_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'num_format': 'yyyy-mm-dd'})
+        default_col1_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': 2})
+        default_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666'})
+        title_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'bottom': 2})
+        level_0_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 6, 'font_color': '#666666'})
+        level_1_col1_total_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 1, 'font_color': '#666666'})
+        level_1_col1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 1, 'font_color': '#666666', 'indent': 1})
+        level_1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 1, 'font_color': '#666666'})
+        level_2_col1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666', 'indent': 2})
+        level_2_col1_total_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666', 'indent': 1})
+        level_2_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666'})
+        col1_styles = {}
+
+        #Set the first column width to 50
+        sheet.set_column(0, 0, 50)
+
+        y_offset = 0
+        headers, lines = self.with_context(no_format=True, print_mode=True, prefetch_fields=False)._get_table(options)
+
+        # Add headers.
+        for header in headers:
+            x_offset = 0
+            for column in header:
+                column_name_formated = column.get('name', '').replace('<br/>', ' ').replace('&nbsp;', ' ')
+                colspan = column.get('colspan', 1)
+                if colspan == 1:
+                    sheet.write(y_offset, x_offset, column_name_formated, title_style)
+                else:
+                    sheet.merge_range(y_offset, x_offset, y_offset, x_offset + colspan - 1, column_name_formated, title_style)
+                x_offset += colspan
+            y_offset += 1
+
+        if options.get('hierarchy'):
+            lines = self.with_context(no_format=True)._create_hierarchy(lines, options)
+        if options.get('selected_column'):
+            lines = self._sort_lines(lines, options)
+
+        # Add lines.
+        for y in range(0, len(lines)):
+            level = lines[y].get('level')
+            is_total_line = 'total' in lines[y].get('class', '').split(' ')
+            if level == 0:
+                y_offset += 1
+                style = level_0_style
+                col1_style = style
+            elif level == 1:
+                style = level_1_style
+                col1_style = level_1_col1_total_style if is_total_line else level_1_col1_style
+            elif level == 2:
+                style = level_2_style
+                col1_style = level_2_col1_total_style if is_total_line else level_2_col1_style
+            elif level == 3:
+                style = default_style
+                level_col1_styles = col1_styles.get(level)
+                if not level_col1_styles:
+                    level_col1_styles = col1_styles[level] = {
+                        'default': workbook.add_format(
+                            {'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': level}
+                        ),
+                        'total': workbook.add_format(
+                            {
+                                'font_name': 'Arial',
+                                'bold': True,
+                                'font_size': 12,
+                                'font_color': '#666666',
+                                'indent': level - 1,
+                            }
+                        ),
+                    }
+                col1_style = level_col1_styles['total'] if is_total_line else level_col1_styles['default']
+            else:
+                style = default_style
+                col1_style = default_col1_style
+
+            #write the first column, with a specific style to manage the indentation
+            cell_type, cell_value = self._get_cell_type_value(lines[y])
+            if cell_type == 'date':
+                sheet.write_datetime(y + y_offset, 0, cell_value, date_default_col1_style)
+            else:
+                sheet.write(y + y_offset, 0, cell_value, col1_style)
+
+            #write all the remaining cells
+            for x in range(1, len(lines[y]['columns']) + 1):
+                cell_type, cell_value = self._get_cell_type_value(lines[y]['columns'][x - 1])
+                if cell_type == 'date':
+                    sheet.write_datetime(y + y_offset, x + lines[y].get('colspan', 1) - 1, cell_value, date_default_style)
+                else:
+                    sheet.write(y + y_offset, x + lines[y].get('colspan', 1) - 1, cell_value, style)
+
+        workbook.close()
+        output.seek(0)
+        generated_file = output.read()
+        output.close()
+
+        return generated_file
